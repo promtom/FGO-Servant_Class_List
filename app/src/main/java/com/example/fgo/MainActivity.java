2d488ece420c7c3262e04d532811f17a8490f81d
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2020-11-23 11:36:13 +0700

    AndroidStudio
    Java

---
+++ app/src/main/java/com/example/fgo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.fgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+    
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        show_list show_list = new show_list(list);
+        rvHeroes.setAdapter(show_list);
+    }
+}
---
+++ app/src/main/java/com/example/fgo/show_list.java
@@ -0,0 +1,56 @@
+package com.example.fgo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class show_list extends RecyclerView.Adapter<show_list.ListViewHolder> {
+    private ArrayList<Hero> listHero;
+    public show_list(ArrayList<Hero> list) {
+        this.listHero = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_row, viewGroup, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        Hero hero = listHero.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+        holder.tvName.setText(hero.getName());
+        holder.tvDetail.setText(hero.getDetail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvDetail;
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvDetail = itemView.findViewById(R.id.tv_item_mean);
+        }
+    }
+}
